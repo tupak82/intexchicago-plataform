dd3d109c1ff4f85d8f0c62c1ad03e1397aa3cf29
tupak82 <diogarciar@gmail.com>
2026-08-22 12:59:09 -0500

    seo: keep admin and api routes out of crawl

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,13 @@ import { site } from "@/lib/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin/", "/api/"],
+      },
+    ],
     sitemap: `${site.url}/sitemap.xml`,
     host: site.url,
   };
