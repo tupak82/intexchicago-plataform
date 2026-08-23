559cbf31dafba57a6a1749747eed6bf9bc9c6b74
tupak82 <diogarciar@gmail.com>
2026-08-22 20:12:48 -0500

    Load roofing service visual system globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { site } from "@/lib/site";
 import "./globals.css";
 import "./brand.css";
 import "./roofing-home.css";
+import "./roofing-service-visuals.css";
 import "./climate-animations.css";
 import "./process-animations.css";
 import "./home-experience-animations.css";
