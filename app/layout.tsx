001441536fa22686a39a2dad6d6947ade92b1642
tupak82 <diogarciar@gmail.com>
2026-08-25 00:34:08 -0500

    Activate cinematic homepage process rail

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ import "./home-services-editorial.css";
 import "./chicago-climate-editorial.css";
 import "./home-climate-editorial.css";
 import "./field-trust-editorial.css";
+import "./home-process-editorial.css";
 import "./service-visual-layout.css";
 import "./site-chrome.css";
 import "./reviews/reviews.css";
@@ -90,7 +91,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <MobileActionBar />
         {googleTagManagerId ? (
           <Script id="google-tag-manager" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${googleTagManagerId}');`}
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${googleTagManagerId}');`}
           </Script>
         ) : null}
         {!googleTagManagerId && googleAnalyticsId ? (
